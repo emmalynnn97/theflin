8a9af48a367968e2813aa82d38584733487797a7
Emma Lynn <emmalynnn97@gmail.com>
2021-04-07 09:19:51 -0700

    update historic copy

--- template-parts/site-historic.php
+++ template-parts/site-historic.php
@@ -5,7 +5,8 @@
 	<div class="row">
 		<div>
             <h3>La Placita Village's History</h3>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime asperiores consequuntur error placeat accusantium, labore nobis dolores quos maiores provident dignissimos itaque dolor pariatur et suscipit, iure esse deleniti ex?</p>
+            <p>La Placita Village opened in 1974 with over 200,000 square feet of mixed-use space. The development was a failed attempt at an urban renewal project which ultimately displaced hundreds of barrio citizens and caused the removal of historical structures. The entire complex encompasses several buildings and structures with historical significance. Among the historical features are the Flin Building and stables, Plaza de la Mesilla, and Samaniego House.
+</p>
             <a class="flin-btn-inverted">Learn More</a>
         </div>
         <img src="/wp-content/uploads/2021/03/plaza_de_la_mesilla.jpg" alt="historic photo"/>
