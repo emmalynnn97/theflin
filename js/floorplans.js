5ec4a27a703f26677bfda2e7a95e6d5617a7e915
Emma Lynn <emmalynnn97@gmail.com>
2021-04-08 10:58:04 -0700

    add in comments

--- js/floorplans.js
+++ js/floorplans.js
@@ -1,25 +1,36 @@
 const modal = document.querySelector("#emma-utils-modal");
 const closeBtn = document.querySelector(".modal-close");
+
+// A method to close the modal
 const closeModal = () => {
     modal.style.display = "none";
 }
-
+// Set close button to close modal on click
 closeBtn.addEventListener('click', closeModal);
+
+// Collect all of the floorplan selectors on the page
 const floorplans = document.querySelectorAll('.floorplan-selector');
 
+// A method to open the modal
 const openModal = () => {
     modal.style.display = "block";
 }
+
+//A method to configure the modal data based on the selected floor plan
 const configureModal = (plan)=>{
     const currData = plan.path[0].dataset;
     document.querySelector('.floorplan-size').innerHTML = `${currData.size} Sq. Ft`;
     document.querySelector('.floorplan-title').innerHTML = currData.title;
     document.querySelector('.floorplan-price').innerHTML = `<span>$</span>${currData.price}/mo`
 }
+
+// Iterate over the floorplans and add event listeners for configuring and opening the modal
 floorplans.forEach(plan=>{
     plan.addEventListener('click', plan=>{configureModal(plan)})
     plan.addEventListener('click', openModal)
 })
+
+// This handles the event in which a user clicks outside of the modal
 window.addEventListener('click', (event) => {
 if (event.target == modal) {
     closeModal();
