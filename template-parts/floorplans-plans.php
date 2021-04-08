49ee24489ddac6def9e22056d64eade5be2132a5
Emma Lynn <emmalynnn97@gmail.com>
2021-04-08 09:57:06 -0700

    floorplan popup functionality

--- template-parts/floorplans-plans.php
+++ template-parts/floorplans-plans.php
@@ -1,3 +1,84 @@
+<style>
+
+/* The Modal (background) */
+#emma-utils-modal {
+    display: none;
+    position: fixed;
+    z-index: 2000;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    background-color: rgb(0, 0, 0);
+    background-color: rgba(0, 0, 0, 0.4);
+}
+/* Styling for close button */
+.modal-close {
+    color: #aaa;
+    font-size: 30px;
+    font-weight: 500;
+    position:absolute;
+    top:0;
+    right:0;
+    }
+    .modal-close:hover,
+    .modal-close:focus {
+    color: #111F2E;
+    text-decoration: none;
+    cursor: pointer;
+}
+.erase-button{
+    border:2px solid black;
+    padding:.75em;
+    position:absolute;
+    left:50%;
+    top:25%;    
+}
+.erase-button:hover{
+    cursor:pointer;
+}
+/* Outer Modal Content Styling */
+.modal-content {
+    position: relative;
+    margin: 7% auto;
+    height:600px;
+    width: 75%;
+    animation-name: animatetop;
+    animation-duration: .75s;
+}
+/* Inner Modal Content Styling */
+.modal-inner {
+    background-color:rgb(255, 245, 255);
+    height:80%;
+    width:100%;
+    padding:1em;
+}
+/* Add Animation */
+@keyframes animatetop {
+    from {top: -600px; opacity: 0}
+    to {top: 0; opacity: 1}
+}
+@media (max-width: 1024px) {
+/* Selectors and styles affecting screens UP TO 1024px (Large Tablet/Desktop) */
+.modal-content{
+    width:80%;
+    height:500px;
+}
+}
+@media (max-width: 768px) {
+/* Selectors and styles affecting screens UP TO 768px (Small Tablet) */
+   .modal-content{
+       height:400px;
+   }
+}
+@media (max-width: 500px) {
+/* Selectors and styles affecting screens UP TO 480px (Phone) */
+   .modal-content{
+       height:300px;
+   }
+}
+</style>
 <div class="vision-floorplans">
 	<div class="row">
 		<div class="triangle triangle-1"></div>
@@ -6,7 +87,7 @@
 	<div class="floorplans-row row">
 		<img src="/wp-content/uploads/2021/03/kitchen_2-1.jpg" alt="floor image"/>
 		<div class="col">
-			<div class="floorplan-selector">
+			<div data-img='' data-title='E1 Floor Plan' data-size='517' data-price='500' class="floorplan-selector">
 				E1 Floor plan
 			</div>
 		</div>
@@ -34,9 +115,6 @@
 			<div class="floorplan-selector">
 				A2.3 Floor plan
 			</div>
-			<div class="floorplan-selector">
-				A2.4 Floor plan
-			</div>
 		</div>
 		<img src="/wp-content/uploads/2021/03/kitchen_2-1.jpg" alt="floor image"/>
 	</div>
@@ -48,18 +126,6 @@
 		<div class="col">
 			<div class="floorplan-selector">
 				B1 Floor plan
-			</div>
-			<div class="floorplan-selector">
-				B1 ALT Floor plan
-			</div>
-			<div class="floorplan-selector">
-				B1 128 Floor plan
-			</div>
-			<div class="floorplan-selector">
-				B1 157 Floor plan
-			</div>
-			<div class="floorplan-selector">
-				B1 108 Floor plan
 			</div>
 			<div class="floorplan-selector">
 				B2 Floor plan
@@ -100,3 +166,59 @@
 		<div class="triangle triangle-2"></div>
 	</div>
 </div>
+<div id="emma-utils-modal">
+        <div class="modal-content">
+		<span class="modal-close">&times;</span>
+		<div class="floorplan">
+			<div class="floorplan-title-wrapper">
+				<h3>One Bedroom</h3>
+			</div>
+			<img src="/wp-content/uploads/2021/02/FPO_Floorplan.png" alt="floorplan image"/>
+			<a class="floorplan-btn">View</a>
+			<div class="floorplan-details">
+				<p>
+					Rent Starting at:
+				</p>
+				<strong>
+					<span>$</span>0000/mo
+				</strong>
+				<p>
+					000 Sq. Ft.
+				</p>
+			</div>
+			<div class="floorplan-features">
+				<p>
+					Features:
+				</p>
+				<ul>
+					<li>Washer/Dryer</li>
+					<li>Laundry Room</li>
+					<li>Walk-in Closet</li>
+					<li>Storage</li>
+				</ul>
+			</div>
+			<a class="floorplan-btn">Download</a>
+			<a class="floorplan-btn-gold">Check Availability</a>
+		</div>
+        </div>
+    </div>
+<script type="text/javascript">
+	const modal = document.querySelector("#emma-utils-modal");
+	const closeBtn = document.querySelector(".modal-close");
+	const closeModal = () => {
+		modal.style.display = "none";
+	}
+	const openModal = () => {
+		modal.style.display = "block";
+	}
+	closeBtn.addEventListener('click', closeModal);
+	const floorplans = document.querySelectorAll('.floorplan-selector');
+	floorplans.forEach(plan=>{
+		plan.addEventListener('click', openModal)
+	})
+	window.addEventListener('click', (event) => {
+    if (event.target == modal) {
+        closeModal();
+    }
+})
+</script>
