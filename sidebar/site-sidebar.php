7bae332a17e8c2a5cdb43f0c3c35f1f57eaacd0f
Emma Lynn <emmalynnn97@gmail.com>
2021-04-09 09:29:26 -0700

    update social links in sidebar

--- sidebar/site-sidebar.php
+++ sidebar/site-sidebar.php
@@ -4,13 +4,12 @@
 	    <span class="close-btn">X</span>
 	</div>
 	<div class="icon-col">
-		<img class="phone" src="/wp-content/uploads/2021/01/phone.png" alt="phone icon"/>
-		<img class="email" src="/wp-content/uploads/2021/01/email.png" alt="email icon"/>
-		<img class="location" src="/wp-content/uploads/2021/01/Location.png" alt="Location icon"/>
-		<img class="facebook" src="/wp-content/uploads/2021/03/facebook-2.png" alt="facebook icon"/>
-		<img class="youtube" src="/wp-content/uploads/2021/03/youtube.png" alt="youtube icon"/>
-		<img class="instagram" src="/wp-content/uploads/2021/03/insta.png" alt="instagram icon"/>
-		<img class="twitter" src="/wp-content/uploads/2021/03/Twitter.png" alt="twitter icon"/>
+		<a href="tel:520.000.000"><img class="phone" src="/wp-content/uploads/2021/01/phone.png" alt="phone icon"/></a>
+		<a href="mailto:theflin@myhslhome.com"><img class="email" src="/wp-content/uploads/2021/01/email.png" alt="email icon"/></a>
+		<a href="https://www.google.com/maps?ll=32.220595,-110.972961&z=14&t=m&hl=en&gl=US&mapclient=embed&daddr=110+S+Church+Ave+Tucson,+AZ+85701@32.220595,-110.9729611"><img class="location" src="/wp-content/uploads/2021/01/Location.png" alt="Location icon"/></a>
+		<a href="https://www.facebook.com/The-Flin-112809383913900"><img class="facebook" src="/wp-content/uploads/2021/03/facebook-2.png" alt="facebook icon"/></a>
+		<a href="https://www.instagram.com/theflin_tucson/"><img class="instagram" src="/wp-content/uploads/2021/03/insta.png" alt="instagram icon"/></a>
+		<a href="https://twitter.com/TheFlin8"><img class="twitter" src="/wp-content/uploads/2021/03/Twitter.png" alt="twitter icon"/></a>
 	</div> 
 	<div class="content-area">
 		<?php get_template_part('sidebar/content', 'sidebar-form') ?>
