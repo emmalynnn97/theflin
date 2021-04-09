8a84ee7d1721765d842a8aafe33d8068087e358d
Emma Lynn <emmalynnn97@gmail.com>
2021-04-09 09:37:19 -0700

    update address on neighborhood page

--- template-parts/neighborhood-full-vertical.php
+++ template-parts/neighborhood-full-vertical.php
@@ -8,7 +8,7 @@
 		</h2>
 	</div>
 
-<iframe width="100%" height="450" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=900&amp;hl=en&amp;q=100%20S%20Church%20Ave+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+	<div style="width: 100%"><iframe width="100%" height="450" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=110%20South%20Church%20Ave.+(The%20Flin)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe></div>
 
 	<div class="row  slideable slideable-right">
 		<a href="https://www.google.com/maps/dir/110+S+Church+Ave,+Tucson,+AZ+85701/Tucson+International+Airport+(TUS),+7250+S+Tucson+Blvd,+Tucson,+AZ+85756/@32.1681318,-110.9823293,13z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x86d670e05d4571a9:0xe83e78775ff5104e!2m2!1d-110.9729611!2d32.220595!1m5!1m1!1s0x86d67b1308d0d5b7:0xb3306c2afc112615!2m2!1d-110.9392269!2d32.1145102" class="col">
