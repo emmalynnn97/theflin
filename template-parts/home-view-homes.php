082f43ab10e1ab6a147ad7874b585d7961797dcc
Emma Lynn <emmalynnn97@gmail.com>
2021-04-12 13:36:08 -0700

    update view homes copy

--- template-parts/home-view-homes.php
+++ template-parts/home-view-homes.php
@@ -5,8 +5,7 @@
 	<div class="row">
 		<div class="text-right content slideable slideable-left">
 			<h3>Our Floor plans Are The Perfect Fit</h3>
-			<h4>22 Floor plans to Choose from, We know you will find one to fit your lifestyle.</h4>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+			<h4>The Flin boasts an array of studio through 3 bedroom floor plans to Choose from, We know you will find one to fit your lifestyle.</h4>
 			<a href="/floorplans" class="flin-btn-inverted">View homes</a>
 		</div>
 		<img class="slideable slideable-right" src='/wp-content/uploads/2021/03/3dfloorplan.png' alt="highrise image"/>
--- template-parts/portal-view-homes.php
+++ template-parts/portal-view-homes.php
@@ -6,7 +6,6 @@
 		<div class="text-right content slideable slideable-left">
 			<h3>Our Floorplans Are The Perfect Fit</h3>
 			<h4>22 Floorplans to Choose from, We know you will find one to fit your lifestyle.</h4>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
 			<a href="/floorplans" class="flin-btn-inverted">View homes</a>
 		</div>
 		<img class="slideable slideable-right" src='/wp-content/uploads/2021/03/3dfloorplan.png' alt="highrise image"/>
