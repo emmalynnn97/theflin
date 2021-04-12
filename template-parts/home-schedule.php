67fa114cfde268fddf828df6f962f4c37feafca6
Emma Lynn <emmalynnn97@gmail.com>
2021-04-12 12:50:08 -0700

    update schedule verbiage to match new opening info

--- template-parts/home-schedule.php
+++ template-parts/home-schedule.php
@@ -1,6 +1,6 @@
 <div class="schedule">
 	<h5>
-		The Flin is opening Summer 2021!<br/>Schedule your private tour to get a look at our <br/> upscale downtown community
+		The Flin is opening late Spring/Summer 2021<br/>Schedule your private tour to get a look at our <br/> upscale downtown community
 	</h5>
 	<?php echo do_shortcode('[gravityform id="3" title="false" description="false"]'); ?>
 </div>
