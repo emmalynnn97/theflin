5e7fd155fb3264d07512fb00453383d74c18908a
Emma Lynn <emmalynnn97@gmail.com>
2021-04-12 12:58:06 -0700

    fix new tab links on neighborhood

--- template-parts/neighborhood-full-vertical.php
+++ template-parts/neighborhood-full-vertical.php
@@ -11,25 +11,25 @@
 	<div style="width: 100%"><iframe width="100%" height="450" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=110%20South%20Church%20Ave.+(The%20Flin)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe></div>
 
 	<div class="row  slideable slideable-right">
-		<a href="https://www.google.com/maps/dir/110+S+Church+Ave,+Tucson,+AZ+85701/Tucson+International+Airport+(TUS),+7250+S+Tucson+Blvd,+Tucson,+AZ+85756/@32.1681318,-110.9823293,13z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x86d670e05d4571a9:0xe83e78775ff5104e!2m2!1d-110.9729611!2d32.220595!1m5!1m1!1s0x86d67b1308d0d5b7:0xb3306c2afc112615!2m2!1d-110.9392269!2d32.1145102" class="col">
+		<a rel="noopener noreferrer" target="_blank" href="https://www.google.com/maps/dir/110+S+Church+Ave,+Tucson,+AZ+85701/Tucson+International+Airport+(TUS),+7250+S+Tucson+Blvd,+Tucson,+AZ+85756/@32.1681318,-110.9823293,13z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x86d670e05d4571a9:0xe83e78775ff5104e!2m2!1d-110.9729611!2d32.220595!1m5!1m1!1s0x86d67b1308d0d5b7:0xb3306c2afc112615!2m2!1d-110.9392269!2d32.1145102" class="col">
 			<img src="/wp-content/uploads/2021/02/Airport.icon_.png" alt='airport image'/>
 			<h3>
 				Airport
 			</h3>
 		</a>
-		<a href="https://www.google.com/maps/search/schools+in+Tucson,+AZ+85701/@32.2187505,-110.9745078,15z/data=!3m1!4b1" class="col">
+		<a rel="noopener noreferrer" target="_blank" href="https://www.google.com/maps/search/schools+in+Tucson,+AZ+85701/@32.2187505,-110.9745078,15z/data=!3m1!4b1" class="col">
 			<img src="/wp-content/uploads/2021/02/School_icon.png" alt='school image'/>
 			<h3>
 				Schools
 			</h3>
 		</a>
-		<a href="https://www.google.com/maps/search/hospitals+in+Tucson,+AZ+85701/@32.2187511,-110.9745078,15z" class="col">
+		<a rel="noopener noreferrer" target="_blank" href="https://www.google.com/maps/search/hospitals+in+Tucson,+AZ+85701/@32.2187511,-110.9745078,15z" class="col">
 			<img src="/wp-content/uploads/2021/02/hosptial_icon.png" alt='Hospital image'/>
 			<h3>
 				Hospital
 			</h3>
 		</a>
-		<a href="https://www.sunlinkstreetcar.com/how-to-ride/maps" class="col">
+		<a rel="noopener noreferrer" target="_blank" href="https://www.sunlinkstreetcar.com/how-to-ride/maps" class="col">
 			<img src="/wp-content/uploads/2021/02/Light_rail_icon.png" alt='light rail image'/>
 			<h3>
 				Light Rail Stops
