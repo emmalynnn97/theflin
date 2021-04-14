b3834900766ff89ef2ba0f110f3a7e8f459f8b8a
Emma Lynn <emmalynnn97@gmail.com>
2021-04-14 09:20:05 -0700

    make floorplan say floor plan again

--- template-parts/portal-view-homes.php
+++ template-parts/portal-view-homes.php
@@ -4,8 +4,8 @@
 	</div>
 	<div class="row">
 		<div class="text-right content slideable slideable-left">
-			<h3>Our Floorplans Are The Perfect Fit</h3>
-			<h4>16 FLOORPLANS TO CHOOSE FROM. WE KNOW YOU WILL FIND ONE TO FIT YOUR LIFESTYLE.</h4>
+			<h3>Our Floor plans Are The Perfect Fit</h3>
+			<h4>16 FLOOR PLANS TO CHOOSE FROM. WE KNOW YOU WILL FIND ONE TO FIT YOUR LIFESTYLE.</h4>
 			<a href="/floorplans" class="flin-btn-inverted">View homes</a>
 		</div>
 		<img class="slideable slideable-right" src='/wp-content/uploads/2021/03/3dfloorplan.png' alt="highrise image"/>
