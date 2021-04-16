44008a790c5ed7f85406d78568084a5f3f82c9ad
Emma Lynn <emmalynnn97@gmail.com>
2021-04-16 08:36:49 -0700

    update home schedule verbiage

--- template-parts/home-schedule.php
+++ template-parts/home-schedule.php
@@ -1,6 +1,7 @@
 <div class="schedule">
 	<h5>
-		The Flin is opening late Spring/Summer 2021<br/>Schedule your private tour to get a look at our <br/> upscale downtown community
+		The Flin is opening late Spring/Summer 2021<br/>VIRTUAL AND SELF-GUIDED TOURS WILL BE AVAILABLE <br/> TO GET A LOOK AT OUR
+UPSCALE DOWNTOWN COMMUNITY
 	</h5>
 	<?php echo do_shortcode('[gravityform id="3" title="false" description="false"]'); ?>
 </div>
