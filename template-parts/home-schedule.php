47080785b43486b31f9b95e8df0b1d78f9833c8a
Emma Lynn <emmalynnn97@gmail.com>
2021-04-19 10:00:19 -0700

    update schedule punctuation

--- template-parts/home-schedule.php
+++ template-parts/home-schedule.php
@@ -1,7 +1,7 @@
 <div class="schedule">
 	<h5>
-		The Flin is opening late Spring/Summer 2021<br/>VIRTUAL AND SELF-GUIDED TOURS WILL BE AVAILABLE <br/> TO GET A LOOK AT OUR
-UPSCALE DOWNTOWN COMMUNITY
+		The Flin is opening late Spring/Summer 2021.<br/>VIRTUAL AND SELF-GUIDED TOURS WILL BE AVAILABLE <br/> TO GET A LOOK AT OUR
+UPSCALE DOWNTOWN COMMUNITY!
 	</h5>
 	<?php echo do_shortcode('[gravityform id="3" title="false" description="false"]'); ?>
 </div>
