60714c77e2c91d75286fb74cfafd698197751aae
Emma Lynn <emmalynnn97@gmail.com>
2021-05-11 12:21:27 -0700

    update footer icons

--- footer.php
+++ footer.php
@@ -21,12 +21,12 @@
 			</div>	
 				<div class="flin-footer-links">
 					<div class="col icon-col">
-					<a href="tel:520.000.000"><img class="phone" src="/wp-content/uploads/2021/01/phone.png" alt="phone icon"/></a>
-		<a href="mailto:theflin@myhslhome.com"><img class="email" src="/wp-content/uploads/2021/01/email.png" alt="email icon"/></a>
-		<a href="https://www.google.com/maps?ll=32.220595,-110.972961&z=14&t=m&hl=en&gl=US&mapclient=embed&daddr=110+S+Church+Ave+Tucson,+AZ+85701@32.220595,-110.9729611"><img class="location" src="/wp-content/uploads/2021/01/Location.png" alt="Location icon"/></a>
-		<a href="https://www.facebook.com/The-Flin-112809383913900"><img class="facebook" src="/wp-content/uploads/2021/03/facebook-2.png" alt="facebook icon"/></a>
-		<a href="https://www.instagram.com/theflin_tucson/"><img class="instagram" src="/wp-content/uploads/2021/03/insta.png" alt="instagram icon"/></a>
-		<a href="https://twitter.com/TheFlin8"><img class="twitter" src="/wp-content/uploads/2021/03/Twitter.png" alt="twitter icon"/></a>
+					<a href="tel:520.000.000"><img class="phone" src="/wp-content/uploads/2021/05/Asset-1-1.png" alt="phone icon"/></a>
+		<a href="mailto:theflin@myhslhome.com"><img class="email" src="/wp-content/uploads/2021/05/Asset-3.png" alt="email icon"/></a>
+		<a href="https://www.google.com/maps?ll=32.220595,-110.972961&z=14&t=m&hl=en&gl=US&mapclient=embed&daddr=110+S+Church+Ave+Tucson,+AZ+85701@32.220595,-110.9729611"><img class="location" src="/wp-content/uploads/2021/05/Asset-2.png" alt="Location icon"/></a>
+		<a href="https://www.facebook.com/The-Flin-112809383913900"><img class="facebook" src="/wp-content/uploads/2021/05/Asset-7.png" alt="facebook icon"/></a>
+		<a href="https://www.instagram.com/theflin_tucson/"><img class="instagram" src="/wp-content/uploads/2021/05/Asset-6.png" alt="instagram icon"/></a>
+		<a href="https://twitter.com/TheFlin8"><img class="twitter" src="/wp-content/uploads/2021/05/Asset-5.png" alt="twitter icon"/></a>
 					</div>
 					<!--<div class="col">
 							<a href="/">Home</a>
