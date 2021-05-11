d46a9e0802c7256e05168a72ecfab14912a5826f
Emma Lynn <emmalynnn97@gmail.com>
2021-05-11 10:43:31 -0700

    update resident portal rentcafe embed

--- template-parts/portal-full.php
+++ template-parts/portal-full.php
@@ -3,7 +3,7 @@
 		<div class="triangle triangle-1"></div>
 	</div>
 	<div class="row">
-		<iframe height=500 width=600 src="https://44monroe.securecafe.com/onlineleasing/44-monroe-0/guestlogin.aspx" title="W3Schools Free Online Web Tutorials">
+		<iframe height=500 width=600 src="https://the-flin-rentcafewebsite.securecafe.com/residentservices/the-flin/userlogin.aspx" title="The Flin Resident Portal">
 </iframe>
 	</div>
 	<div class="row">
