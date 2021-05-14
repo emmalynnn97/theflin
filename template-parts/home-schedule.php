c800dd109deb8fd9b3b918e73272d36da4aeb0b8
Emma Lynn <emmalynnn97@gmail.com>
2021-05-14 12:53:30 -0700

    update home schedule insert verbiage

--- template-parts/home-schedule.php
+++ template-parts/home-schedule.php
@@ -1,6 +1,6 @@
 <div class="schedule">
 	<h5>
-		The Flin is opening late Spring/Summer 2021.<br/>To learn more about our community<br/> submit your information below.
+		The Flin is OPENING THIS SUMMER!<br/>To learn more about our community<br/> submit your information below.
 	</h5>
 	<?php echo do_shortcode('[gravityform id="3" title="false" description="false"]'); ?>
 </div>
