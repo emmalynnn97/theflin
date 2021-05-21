eddba618a4086ef075ef5abd4960678b46be0324
Emma Lynn <emmalynnn97@gmail.com>
2021-05-21 14:27:51 -0700

    give history page overlay

--- template-parts/history-top.php
+++ template-parts/history-top.php
@@ -1,8 +1,11 @@
 <div class="history-top">
-		<h1>
+<div class="history-top-overlay">
+<h1>
 			<?php echo the_title(); ?>
 		</h1>
 		<a href="/floorplans" class="flin-btn">
 			View Homes
 		</a>
+</div>		
+
 </div>
