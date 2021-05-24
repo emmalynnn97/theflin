d08ec11f79acf9e8c4b9e9cc7c4899d1a46c6b12
Emma Lynn <emmalynnn97@gmail.com>
2021-05-24 13:39:05 -0700

    fix copy on view homes inserts

--- template-parts/neighborhood-view-homes.php
+++ template-parts/neighborhood-view-homes.php
@@ -5,7 +5,7 @@
 	<div class="row">
 		<div class="text-right content slideable slideable-left">
 			<h3>Our Floor plans Are The Perfect Fit</h3>
-			<h4>The Flin boasts an array of studio through 3 bedroom floor plans to Choose from. we know you will find one to fit your lifestyle.</h4>
+			<h4>The Flin boasts an array of studio through 3-bedroom floor plans to Choose from. we know you will find one to fit your lifestyle.</h4>
 			<a href="/floorplans" class="flin-btn-inverted">View homes</a>
 		</div>
 		<img class="slideable slideable-right" src='/wp-content/uploads/2021/03/3dfloorplan.png' alt="highrise image"/>
